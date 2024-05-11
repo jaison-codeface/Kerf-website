460081525dba0240076e494335cd3ff8e1051395
MdodoIng <contentful.speakerbox@gmail.com>
2024-05-11 10:26:37 +0530

    modified:   components/layout/index.tsx

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,16 +1,20 @@
 "use client";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useLayoutEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
-AOS.init({
-  once: true,
-});
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  useLayoutEffect(() => {
+    if (typeof document !== "undefined") {
+      AOS.init({
+        once: true,
+      });
+    }
+  }, []);
   return (
     <main className="flex flex-col w-full flex-1 items-center justify-center text-tertiary relative z-0 bg-white">
       <Header />
