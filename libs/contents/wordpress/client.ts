b8a7fea461965fff19246a53b04e1580b06f2715
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-06-19 14:26:35 +0530

    new file:   libs/contents/wordpress/client.ts 	new file:   libs/contents/wordpress/data.tsx 	new file:   libs/contents/wordpress/queries/index.ts

---
+++ libs/contents/wordpress/client.ts
@@ -0,0 +1,4 @@
+const API_URL = "https://cfuat.in/lgadxb/graphql";
+const WORDPRESS_AUTH_REFRESH_TOKEN = "";
+
+export { API_URL, WORDPRESS_AUTH_REFRESH_TOKEN };
---
+++ libs/contents/wordpress/data.tsx
@@ -0,0 +1,34 @@
+import { API_URL, WORDPRESS_AUTH_REFRESH_TOKEN } from "./client";
+
+async function fecthAPi(query = "") {
+  const headers = { "Content-Type": "application/json" };
+  if (WORDPRESS_AUTH_REFRESH_TOKEN) {
+    Headers["Authorization"] = `Bearer ${WORDPRESS_AUTH_REFRESH_TOKEN}`;
+  }
+
+  // WPGraphQL Plugin must be enabled
+  const res = await fetch(API_URL, {
+    headers,
+    method: "POST",
+    body: JSON.stringify({
+      query,
+    }),
+    cache: "no-store",
+  });
+  const json = await res.json();
+  if (json.errors) {
+    console.error(json.errors);
+    throw new Error("Failed to fetch API");
+  }
+  return json.data;
+}
+
+type Ids = { "": string };
+
+export async function getContentFromWordPress(id: Ids, slug?: string) {
+  const query: Ids = {
+    "": "",
+  };
+  const data = await fecthAPi(query[id]);
+  return data;
+}
