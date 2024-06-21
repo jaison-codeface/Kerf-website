45762c92158c554c16067ccd99defe664671515a
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-06-21 12:01:42 +0530

    new file:   assets/fonts/GoogleSans-Medium.woff2 modified:   libs/font.ts

--- libs/font.ts
+++ libs/font.ts
@@ -3,30 +3,35 @@ import LocalFont from "next/font/local";
 export const googleSans = LocalFont({
   src: [
     {
-      path: "../assets/fonts/GoogleSans-Bold.woff2",
-      weight: "700",
+      path: "../assets/fonts/GoogleSans-Regular.woff2",
+      weight: "400",
       style: "normal",
     },
     {
-      path: "../assets/fonts/GoogleSans-BoldItalic.woff2",
+      path: "../assets/fonts/GoogleSans-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/GoogleSans-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
       path: "../assets/fonts/GoogleSans-Italic.woff2",
       weight: "400",
-      style:'italic',
+      style: "italic",
     },
     {
       path: "../assets/fonts/GoogleSans-MediumItalic.woff2",
       weight: "500",
-      style: "normal",
+      style: "italic",
     },
     {
-      path: "../assets/fonts/GoogleSans-Regular.woff2",
-      weight: "400",
-      style: "normal",
+      path: "../assets/fonts/GoogleSans-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
     },
   ],
-  variable : '--font--google-sans'
+  variable: "--font--google-sans",
 });
