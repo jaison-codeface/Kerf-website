ddfc1a41ed7581826978939327824bec948e8ab3
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-06-21 21:19:47 +0530

    modified:   components/departments/[slug]/Content.tsx

--- components/departments/[slug]/Content.tsx
+++ components/departments/[slug]/Content.tsx
@@ -7,19 +7,18 @@ import { BookAppointmentBtn } from "@/ui/buttons";
 import WordPressRichText from "@/components/WordPressRichText";
 
 const Content = ({ data }: { data: DepartmentsType }) => {
-  const isData = data.departments.edges[0].node;
   return (
     <SectionWrapper classBottom={`${main_padding.y} flex-col items-center`}>
       <Image
-        src={isData.acf.bannerImage.sourceUrl}
-        alt={isData.acf.bannerImage.altText}
+        src={data.acf.bannerImage.sourceUrl}
+        alt={data.acf.bannerImage.altText}
         data-aos="fade-up"
         width={1200}
         height={600}
         className="md:aspect-[16/7] aspect-video rounded-xl overflow-hidden object-cover md:w-10/12"
       />
       <div className=" flex flex-col md:w-9/12 mt-16">
-        <WordPressRichText htmlString={isData.acf.content} />
+        <WordPressRichText htmlString={data.acf.content} />
         <BookAppointmentBtn />
       </div>
     </SectionWrapper>
