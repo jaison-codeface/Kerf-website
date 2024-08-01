91cdce74a723947af346a00fdc1733d30dbf49db
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-08-01 12:52:28 +0530

    SFDSF

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,61 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://acme.com",
+      url: "https://www.kerfenthospital.com/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://acme.com/about",
+      url: "https://www.kerfenthospital.com/about_us",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://acme.com/blog",
+      url: "https://www.kerfenthospital.com/blogs",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.kerfenthospital.com/contact_us",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.kerfenthospital.com/departments",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.kerfenthospital.com/doctors",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.kerfenthospital.com/international-patient-care",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.kerfenthospital.com/privacy-and-policy",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.kerfenthospital.com/terms-and-conditions",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.kerfenthospital.com/treatments",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
