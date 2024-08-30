573fd013e765a647168e07f6047825dfb57a5fac
dodo <110053481+MdodoIng@users.noreply.github.com>
2024-08-30 10:21:09 +0530

    zfds

--- components/Home/Surgery.tsx
+++ components/Home/Surgery.tsx
@@ -48,34 +48,38 @@ const Surgery = ({ data }: { data: HomePageType }) => {
       <HeadSubtitle className="">{isData.subtitle}</HeadSubtitle>
       <HeadTitle className="mt-1 ">{isData.title}</HeadTitle>
       <div className="grid md:grid-cols-3 md:gap-10 gap-8 w-full mt-8">
-        {isData.content.map((item, idx) => (
-          <div
-            key={idx}
-            data-aos="fade-up"
-            data-aos-delay={idx * 50}
-            className="flex items-start flex-col justify-start gap-8 bg-kerf-teal p-4 rounded-xl text-white pb-8 duration-500 group hover:!rotate-1">
-            <div className="h-auto aspect-video w-full  relative z-0  overflow-hidden rounded-lg ">
-              <Image
-                src={item.image.sourceUrl}
-                alt=""
-                fill
-                className=" object-cover inset-0  size-full"
-              />
+        {isData.content &&
+          isData.content.map((item, idx) => (
+            <div
+              key={idx}
+              data-aos="fade-up"
+              data-aos-delay={idx * 50}
+              className="flex items-start flex-col justify-start gap-8 bg-kerf-teal p-4 rounded-xl text-white pb-8 duration-500 group hover:!rotate-1">
+              {item.image.sourceUrl && (
+                <div className="h-auto aspect-video w-full  relative z-0  overflow-hidden rounded-lg ">
+                  <Image
+                    src={item.image.sourceUrl}
+                    alt=""
+                    fill
+                    className=" object-cover inset-0  size-full"
+                  />
+                </div>
+              )}
+              <div className="flex w-full justify-center  flex-col gap-4">
+                <h2 className="font-bold text-3xl text-white">{item.title}</h2>
+                <menu className="flex items-start justify-start flex-wrap gap-2">
+                  {item.surgeries &&
+                    item.surgeries.map((i, d) => (
+                      <li
+                        key={d}
+                        className="px-3 py-1 text-base bg-[#05a5a5] text-white  rounded-lg overflow-hidden">
+                        {i.title}
+                      </li>
+                    ))}
+                </menu>
+              </div>
             </div>
-            <div className="flex w-full justify-center  flex-col gap-4">
-              <h2 className="font-bold text-3xl text-white">{item.title}</h2>
-              <menu className="flex items-start justify-start flex-wrap gap-2">
-                {item.surgeries.map((i, d) => (
-                  <li
-                    key={d}
-                    className="px-3 py-1 text-base bg-[#05a5a5] text-white  rounded-lg overflow-hidden">
-                    {i.title}
-                  </li>
-                ))}
-              </menu>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
       <RosePetalTexture className="-bottom-32 right-0 rotate-180 -z-10" />
     </SectionWrapper>
