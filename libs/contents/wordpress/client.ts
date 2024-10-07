420ca6e6a1d7532228146adbac10fdbb1e4293bc
jaison-codeface <jaison@codefacetech.com>
2024-10-08 00:34:36 +0530

    Update client.ts

--- libs/contents/wordpress/client.ts
+++ libs/contents/wordpress/client.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://admin.kerfenthospital.com/graphql";
+const API_URL = "https://kerf.ddhotel.in/graphql";
 const WORDPRESS_AUTH_REFRESH_TOKEN = "";
 
 export { API_URL, WORDPRESS_AUTH_REFRESH_TOKEN };
